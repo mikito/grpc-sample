0615b5689499a8a1c51de6d5bc82e487181266b7
yoshiya <yoshiya@WP-PC-2021-054.local>
2024-11-11 13:12:03 +0900

    reflection

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from concurrent import futures
 import grpc
 import service_pb2
 import service_pb2_grpc
+from grpc_reflection.v1alpha import reflection
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -20,6 +21,15 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     service_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     service_pb2_grpc.add_HealthServicer_to_server(HealthServicer(), server)
+
+    # Reflection serviceの追加
+    SERVICE_NAMES = [
+        service_pb2.DESCRIPTOR.services_by_name['Greeter'].full_name,
+        service_pb2.DESCRIPTOR.services_by_name['Health'].full_name,
+        reflection.SERVICE_NAME,
+    ]
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
     server.add_insecure_port('[::]:9501')
     server.start()
     server.wait_for_termination()
