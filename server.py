946008f59a70331b69c553c3751d12f6553095a2
yoshiya <yoshiya@WP-PC-2021-054.local>
2024-11-11 11:20:21 +0900

    ログを出す

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
+import logging
 from concurrent import futures
 import grpc
 import service_pb2
 import service_pb2_grpc
 
+# ロギングの設定
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Greeter(service_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
+        logging.info(f"Received a request with name: {request.name}")
         return service_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 def serve():
