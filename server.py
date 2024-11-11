5a4c572a19a569eb9d288516acce96e0a118b628
yoshiya <yoshiya@WP-PC-2021-054.local>
2024-11-11 12:16:44 +0900

    health check

--- server.py
+++ server.py
@@ -4,18 +4,22 @@ import grpc
 import service_pb2
 import service_pb2_grpc
 
-# ロギングの設定
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Greeter(service_pb2_grpc.GreeterServicer):
-
     def SayHello(self, request, context):
         logging.info(f"Received a request with name: {request.name}")
         return service_pb2.HelloReply(message='Hello, %s!' % request.name)
 
+class HealthServicer(service_pb2_grpc.HealthServicer):
+    def Check(self, request, context):
+        logging.info(f"Received a health request")
+        return service_pb2.HealthCheckResponse(status=service_pb2.HealthCheckResponse.SERVING)
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     service_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    service_pb2_grpc.add_HealthServicer_to_server(HealthServicer(), server)
     server.add_insecure_port('[::]:9501')
     server.start()
     server.wait_for_termination()
